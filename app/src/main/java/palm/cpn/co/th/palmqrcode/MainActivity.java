d82090b1893707980e6275c111f74013667e0f16
Suwadee <piecustard@gmail.com>
2018-03-07 15:34:23 +0700

    Create MainFragment Success

--- app/src/main/java/palm/cpn/co/th/palmqrcode/MainActivity.java
+++ app/src/main/java/palm/cpn/co/th/palmqrcode/MainActivity.java
@@ -3,11 +3,21 @@ package palm.cpn.co.th.palmqrcode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import palm.cpn.co.th.palmqrcode.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+
+    }   // Main Method
+}  // Main Class
---
+++ app/src/main/java/palm/cpn/co/th/palmqrcode/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package palm.cpn.co.th.palmqrcode.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import palm.cpn.co.th.palmqrcode.R;
+
+/**
+ * Created by chsuwadee on 07/03/2561.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        return view;
+    }
+}   // Main Class
