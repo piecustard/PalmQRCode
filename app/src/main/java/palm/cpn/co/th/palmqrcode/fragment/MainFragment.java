ef74f48723444350c2d374fc0b6ad8712722f9fb
Suwadee <piecustard@gmail.com>
2018-03-08 09:46:56 +0700

    Replace Register Layout

--- app/src/main/java/palm/cpn/co/th/palmqrcode/fragment/MainFragment.java
+++ app/src/main/java/palm/cpn/co/th/palmqrcode/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import palm.cpn.co.th.palmqrcode.R;
 
@@ -15,10 +16,36 @@ import palm.cpn.co.th.palmqrcode.R;
 
 public class MainFragment extends Fragment{
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+    }   // Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment,new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main,container,false);
         return view;
-    }
+    }   // Method Create Mask
 }   // Main Class
---
+++ app/src/main/java/palm/cpn/co/th/palmqrcode/fragment/RegisterFragment.java
@@ -0,0 +1,24 @@
+package palm.cpn.co.th.palmqrcode.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import palm.cpn.co.th.palmqrcode.R;
+
+/**
+ * Created by Lorien on 08/03/2018.
+ */
+
+public class RegisterFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        return view;
+    }
+}
