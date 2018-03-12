deefddfd77018f342b7f6b04023dfa9151c6064d
Suwadee <piecustard@gmail.com>
2018-03-12 13:17:54 +0700

    Drawer and Hamberger OK

--- app/src/main/java/palm/cpn/co/th/palmqrcode/ServiceActivity.java
+++ app/src/main/java/palm/cpn/co/th/palmqrcode/ServiceActivity.java
@@ -1,15 +1,21 @@
 package palm.cpn.co.th.palmqrcode;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 
 public class ServiceActivity extends AppCompatActivity {
     //    Explicit
     private String tag = "12MarchV1";
     private String[] loginStrings; // ตัวแปรที่มารับค่า Login
-
-
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle; // การทำ Toggle บน toolbar
 
 
 
@@ -19,11 +25,59 @@ public class ServiceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_service);
 
 //        Get Value From Intent
-        loginStrings = getIntent().getStringArrayExtra("Login");
-        Log.d(tag, "NameLogin ==> " + loginStrings[1]);
+        getValueFromIntent();
+
+//        Create Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarService);
+        setSupportActionBar(toolbar);
+
+//        Setup Title
+        getSupportActionBar().setTitle("My Service");
+        getSupportActionBar().setSubtitle(loginStrings[1]);
+
+        getSupportActionBar().setHomeButtonEnabled(true);       // ขอเปิดใช้บริการปุ่ม Home
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);  //ขอให้ปุ่ม Home Click ได้
+
+//        Create Hamberger Icon
+        drawerLayout = findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(ServiceActivity.this,
+                drawerLayout,R.string.open,R.string.close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
     }   // Main Method
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+
+    }
+
+
+    private void getValueFromIntent() {
+        loginStrings = getIntent().getStringArrayExtra("Login");
+        Log.d(tag, "NameLogin ==> " + loginStrings[1]);
+    }
 
 
 }   // Main Class
