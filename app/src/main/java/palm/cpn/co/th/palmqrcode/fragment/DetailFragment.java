272b8e6508734beb64f6b1338206a8375ba4e6ee
Suwadee <piecustard@gmail.com>
2018-03-13 09:52:06 +0700

    Read All Data Success

--- app/src/main/java/palm/cpn/co/th/palmqrcode/fragment/DetailFragment.java
+++ app/src/main/java/palm/cpn/co/th/palmqrcode/fragment/DetailFragment.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import palm.cpn.co.th.palmqrcode.R;
 
@@ -40,9 +45,50 @@ public class DetailFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+//        Show All
+        showAll();
 
+
+//        Back Controller
+        backController();
 
     }   // Main Method
+
+    private void backController() {
+        Button button = getView().findViewById(R.id.btnShowListView);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 getActivity().getSupportFragmentManager().popBackStack(); //ทำการสลายตัวเอง กลับไปตัวเก่า (เหมือน Finish , Exit)
+            }
+        });
+    }
+
+    private void showAll() {
+//        Title
+        TextView titleTextView = getView().findViewById(R.id.txttitle);
+        String titleString = getArguments().getString("NameFood", "Nothing");
+        titleTextView.setText(titleString);
+
+//        Category
+        TextView categoryTextView = getView().findViewById(R.id.txtCategory);
+        categoryTextView.setText(getArguments().getString("Category"));
+
+//        Price
+        TextView priceTextView = getView().findViewById(R.id.txtPrice);
+        priceTextView.setText(getArguments().getString("Price"));
+
+//        Detail
+        TextView detailTextView = getView().findViewById(R.id.txtDetail);
+        detailTextView.setText(getArguments().getString("Detail"));
+
+//        Image
+        ImageView imageView = getView().findViewById(R.id.imvImage);
+        Picasso.get()
+                .load(getArguments().getString("ImagePath","http://androidthai.in.th/mua/lmage/food1.jpg"))
+                .into(imageView);
+
+    }
 
 
     @Nullable
